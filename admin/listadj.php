b7030ebaac38d49c11c02f2273af7613062106f5
wallenium <wallenium@eqdkp-plus.eu>
2012-12-03 22:42:32 +0000

    move tags folder to core/tags

---
+++ admin/listadj.php
@@ -0,0 +1,136 @@
+<?php
+/******************************
+ * EQdkp
+ * Copyright 2002-2003
+ * Licensed under the GNU GPL.  See COPYING for full terms.
+ * ------------------
+ * listadj.php
+ * Began: Fri December 27 2002
+ * 
+ * $Id$
+ * 
+ ******************************/
+
+define('EQDKP_INC', true);
+define('IN_ADMIN', true);
+$eqdkp_root_path = './../';
+include_once($eqdkp_root_path . 'common.php');
+
+$sort_order = array(
+    0 => array('adjustment_date desc', 'adjustment_date'),
+    1 => array('member_name', 'member_name desc'),
+    2 => array('adjustment_reason', 'adjustment_reason desc'),
+    3 => array('adjustment_value desc', 'adjustment_value'),
+    4 => array('adjustment_added_by', 'adjustment_added_by desc'),
+    5 => array('raid_name', 'raid_name desc')
+);
+
+$current_order = switch_order($sort_order);
+
+//
+// Group Adjustments
+//
+if ( (!isset($_GET[URI_PAGE])) || ($_GET[URI_PAGE] == 'group') )
+{
+    $user->check_auth('a_groupadj_');
+    
+    $u_list_adjustments = 'listadj.php'.$SID.'&amp;';
+    
+    $page_title = sprintf($user->lang['admin_title_prefix'], $eqdkp->config['guildtag'], $eqdkp->config['dkp_name']).': '.$user->lang['listadj_title'];
+    
+    $total_adjustments = $db->query_first('SELECT count(*) FROM ' . ADJUSTMENTS_TABLE . ' WHERE member_name IS NULL');
+    $start = ( isset($_GET['start']) ) ? $_GET['start'] : 0;
+    
+    $s_group_adj = true;
+    
+    $sql = 'SELECT adjustment_id, adjustment_value, adjustment_date, adjustment_added_by, raid_name
+            FROM ' . ADJUSTMENTS_TABLE . '
+            WHERE member_name IS NULL
+            ORDER BY '.$current_order['sql'].'
+            LIMIT '.$start.','.$user->data['user_alimit'];
+    
+    $listadj_footcount = sprintf($user->lang['listadj_footcount'], $total_adjustments, $user->data['user_alimit']);
+    $pagination = generate_pagination('listadj.php'.$SID.'&amp;o='.$current_order['uri']['current'],
+                                       $total_adjustments, $user->data['user_alimit'], $start);
+}
+
+//
+// Individual Adjustments
+//
+elseif ( $_GET[URI_PAGE] == 'individual' )
+{
+    $user->check_auth('a_indivadj_');
+    
+    $u_list_adjustments = 'listadj.php'.$SID.'&amp;' . URI_PAGE . '=individual&amp;';
+    
+    $page_title = sprintf($user->lang['admin_title_prefix'], $eqdkp->config['guildtag'], $eqdkp->config['dkp_name']).': '.$user->lang['listiadj_title'];
+    
+    $total_adjustments = $db->query_first('SELECT count(*) FROM ' . ADJUSTMENTS_TABLE . ' WHERE member_name IS NOT NULL');
+    $start = ( isset($_GET['start']) ) ? $_GET['start'] : 0;
+    
+    $s_group_adj = false;
+    
+    $sql = 'SELECT adjustment_id, adjustment_value, member_name, adjustment_reason, adjustment_date, adjustment_added_by, raid_name
+            FROM ' . ADJUSTMENTS_TABLE . '
+            WHERE member_name IS NOT NULL
+            ORDER BY '.$current_order['sql'].'
+            LIMIT '.$start.','.$user->data['user_alimit'];
+    
+    $listadj_footcount = sprintf($user->lang['listiadj_footcount'], $total_adjustments, $user->data['user_alimit']);
+    $pagination = generate_pagination('listadj.php'.$SID.'&amp;' . URI_PAGE . '=individual&amp;o='.$current_order['uri']['current'],
+                                       $total_adjustments, $user->data['user_alimit'], $start);
+}
+
+if ( !($adj_result = $db->query($sql)) )
+{
+    message_die('Could not obtain adjustment information', '', __FILE__, __LINE__, $sql);
+}
+
+while ( $adj = $db->fetch_record($adj_result) )
+{
+    $tpl->assign_block_vars('adjustments_row', array(
+        'ROW_CLASS' => $eqdkp->switch_row_class(),
+        'U_ADD_ADJUSTMENT' => (( $s_group_adj ) ? 'addadj.php' : 'addiadj.php') . $SID.'&amp;' . URI_ADJUSTMENT . '='.$adj['adjustment_id'],
+        'DATE' => date($user->style['date_notime_short'], $adj['adjustment_date']),
+        'U_VIEW_MEMBER' => ( isset($adj['member_name']) ) ? $eqdkp_root_path.'viewmember.php'.$SID.'&amp;' . URI_NAME . '='.$adj['member_name'] : '',
+        'MEMBER' => ( isset($adj['member_name']) ) ? $adj['member_name'] : '',
+        'REASON' => ( isset($adj['adjustment_reason']) ) ? stripslashes($adj['adjustment_reason'])  : '',
+        'ADJUSTMENT' => $adj['adjustment_value'],
+        'C_ADJUSTMENT' => color_item($adj['adjustment_value']),
+        'RAID_NAME' => $adj['raid_name'],
+        'ADDED_BY' => ( isset($adj['adjustment_added_by']) ) ? $adj['adjustment_added_by'] : '')
+    );
+}
+$db->free_result($adj_result);
+
+$tpl->assign_vars(array(
+    'L_DATE' => $user->lang['date'],
+    'L_MEMBER' => $user->lang['member'],
+    'L_REASON' => $user->lang['reason'],
+    'L_ADJUSTMENT' => $user->lang['adjustment'],
+    'L_ADDED_BY' => $user->lang['added_by'],
+    'L_MULTI_EVENTNAME' => $user->lang['Multi_eventname'],
+    
+    
+    
+    'O_DATE' => $current_order['uri'][0],
+    'O_MEMBER' => $current_order['uri'][1],
+    'O_RAID' => $current_order['uri'][5],
+    'O_REASON' => $current_order['uri'][2],
+    'O_ADJUSTMENT' => $current_order['uri'][3],
+    'O_ADDED_BY' => $current_order['uri'][4],
+    
+    'U_LIST_ADJUSTMENTS' => $u_list_adjustments,
+    
+    'START' => $start,
+    'S_GROUP_ADJ' => $s_group_adj,
+    'LISTADJ_FOOTCOUNT' => $listadj_footcount,
+    'ADJUSTMENT_PAGINATION' => $pagination)
+);
+
+$eqdkp->set_vars(array(
+    'page_title'    => $page_title,
+    'template_file' => 'admin/listadj.html',
+    'display'       => true)
+);
+?>
---
+++ admin/listitems.php
@@ -0,0 +1,91 @@
+<?php
+/******************************
+ * EQdkp
+ * Copyright 2002-2003
+ * Licensed under the GNU GPL.  See COPYING for full terms.
+ * ------------------
+ * listitems.php
+ * Began: Fri December 27 2002
+ *
+ * $Id$
+ *
+ ******************************/
+
+define('EQDKP_INC', true);
+define('IN_ADMIN', true);
+$eqdkp_root_path = './../';
+include_once($eqdkp_root_path . 'common.php');
+include_once($eqdkp_root_path . 'itemstats/eqdkp_itemstats.php');
+
+$user->check_auth('a_item_');
+
+$sort_order = array(
+    0 => array('item_date desc', 'item_date'),
+    1 => array('item_buyer', 'item_buyer desc'),
+    2 => array('item_name', 'item_name desc'),
+    3 => array('raid_name', 'raid_name desc'),
+    4 => array('item_value desc', 'item_value')
+);
+
+$current_order = switch_order($sort_order);
+
+$total_items = $db->query_first('SELECT count(*) FROM ' . ITEMS_TABLE);
+$start = ( isset($_GET['start']) ) ? $_GET['start'] : 0;
+
+$sql = 'SELECT i.item_id, i.item_name, i.item_buyer, i.item_date, i.raid_id, i.item_value, r.raid_name
+        FROM ' . ITEMS_TABLE . ' i, ' . RAIDS_TABLE . ' r
+        WHERE r.raid_id=i.raid_id
+        ORDER BY '.$current_order['sql']. '
+        LIMIT '.$start.','.$user->data['user_ilimit'];
+
+$listitems_footcount = sprintf($user->lang['listpurchased_footcount'], $total_items, $user->data['user_ilimit']);
+$pagination = generate_pagination('listitems.php'.$SID.'&amp;o='.$current_order['uri']['current'], $total_items, $user->data['user_ilimit'], $start);
+
+if ( !($items_result = $db->query($sql)) )
+{
+    message_die('Could not obtain item information', 'Database error', __FILE__, __LINE__, $sql);
+}
+
+while ( $item = $db->fetch_record($items_result) )
+{
+    $tpl->assign_block_vars('items_row', array(
+        'ROW_CLASS' => $eqdkp->switch_row_class(),
+        'DATE' => ( !empty($item['item_date']) ) ? date($user->style['date_notime_short'], $item['item_date']) : '&nbsp;',
+        'BUYER' => ( !empty($item['item_buyer']) ) ? $item['item_buyer'] : '&lt;<i>Not Found</i>&gt;',
+        'U_VIEW_BUYER' => ( !empty($item['item_buyer']) ) ? '../viewmember.php'.$SID.'&amp;' . URI_NAME . '='.$item['item_buyer'] : '',
+        'NAME' => itemstats_decorate_name(stripslashes($item['item_name'])),
+        'U_VIEW_ITEM' => 'additem.php'.$SID.'&amp;' . URI_ITEM . '='.$item['item_id'],
+        'RAID' => ( !empty($item['raid_name']) ) ? stripslashes($item['raid_name']) : '&lt;<i>Not Found</i>&gt;',
+        'U_VIEW_RAID' => ( !empty($item['raid_name']) ) ? 'addraid.php'.$SID.'&amp;' . URI_RAID . '='.$item['raid_id'] : '',
+        'VALUE' => $item['item_value'])
+    );
+}
+$db->free_result($items_result);
+
+$tpl->assign_vars(array(
+    'L_DATE' => $user->lang['date'],
+    'L_BUYER' => $user->lang['buyer'],
+    'L_ITEM' => $user->lang['item'],
+    'L_RAID' => $user->lang['raid'],
+    'L_VALUE' => $user->lang['value'],
+
+    'O_DATE' => $current_order['uri'][0],
+    'O_BUYER' => $current_order['uri'][1],
+    'O_NAME' => $current_order['uri'][2],
+    'O_RAID' => $current_order['uri'][3],
+    'O_VALUE' => $current_order['uri'][4],
+
+    'U_LIST_ITEMS' => 'listitems.php'.$SID.'&amp;',
+
+    'START' => $start,
+    'S_HISTORY' => true,
+    'LISTITEMS_FOOTCOUNT' => $listitems_footcount,
+    'ITEM_PAGINATION' => $pagination)
+);
+
+$eqdkp->set_vars(array(
+    'page_title'    => sprintf($user->lang['admin_title_prefix'], $eqdkp->config['guildtag'], $eqdkp->config['dkp_name']).': '.$user->lang['listpurchased_title'],
+    'template_file' => 'listitems.html',
+    'display'       => true)
+);
+?>
