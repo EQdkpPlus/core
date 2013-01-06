47d524bab42ed11e5c5151781c4d727580185357
godmod <godmod@eqdkp-plus.eu>
2013-01-06 14:47:38 +0000

    added some games

---
+++ games/raiderz/language/english.php
@@ -0,0 +1,43 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2011
+ * Date:		$Date: 2011-12-01 08:09:31 +0100 (Thu, 01 Dec 2011) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: wallenium $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 11484 $
+ * 
+ * $Id: english.php 11484 2011-12-01 07:09:31Z wallenium $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+$english_array =  array(
+	'classes' => array(
+		0	=> 'Unknown',
+		1	=> 'Defender',
+		2	=> 'Berzerker',
+		3	=> 'Cleric',
+		4	=> 'Sorcerer',
+	),
+
+	'races' => array(
+				'Unknown',
+				'Human',
+	),
+	'factions' => array(
+		'Default',
+	),
+	'lang' => array(
+		'raiderz' => 'RaiderZ',
+	),
+);
+
+?>
