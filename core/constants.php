25e9a590c36a28d80ff7b8e8006882eb68e6bf04
wallenium <wallenium@eqdkp-plus.eu>
2013-01-07 00:18:38 +0000

    - update internal version number to right value

--- core/constants.php
+++ core/constants.php
@@ -25,7 +25,7 @@ if ( !defined('EQDKP_INC') ){
  * CORE VERSIONS
  * -------------------------------------------------------------------------
  */
-define('VERSION_INT',		'1.0.8');				// the internal version number for updates & update check
+define('VERSION_INT',		'1.0.9');				// the internal version number for updates & update check
 define('VERSION_EXT',		'1.0 RC 2');			// the external version number to be shown in the footer
 define('VERSION_WIP',		false);					// work in progress or stable?
 define('VERSION_PHP_RQ',	'5.2.0');				// required version of PHP
