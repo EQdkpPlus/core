a5428e07912c3d0e6e171eed91c772bd59945522
wallenium <wallenium@eqdkp-plus.eu>
2013-01-08 23:40:18 +0000

    - better bars for bosses :)

--- games/wow/profiles/profile_additions.php
+++ games/wow/profiles/profile_additions.php
@@ -543,14 +543,19 @@
 											</tr>';
 					}
 					$tt_bossprogress .= '</table>';
-	
+
+					// normal
+					$percent_bc_normal	= ($v_bossprogress['bosses_normal'] != 0) ? intval(($v_bossprogress['bosses_normal'] / $v_bossprogress['bosses_max']) * 100) : 0;
+					$bar_bc_normal		= $this->jquery->ProgressBar('bcnormal_'.$v_bossprogress['id'], $percent_bc_normal, $v_bossprogress['bosses_normal'] .' / ' . $v_bossprogress['bosses_max'].' ('.$percent_bc_normal.'%)');
+
+					//Heroic
+					$percent_bc_heroic	= ($v_bossprogress['bosses_heroic'] != 0) ? intval(($v_bossprogress['bosses_heroic'] / $v_bossprogress['bosses_max']) * 100) : 0;
+					$bar_bc_heroic		= $this->jquery->ProgressBar('bcheroic_'.$v_bossprogress['id'], $percent_bc_heroic, 'heroic '.$v_bossprogress['bosses_heroic'] .' / ' . $v_bossprogress['bosses_max'].' ('.$percent_bc_heroic.'%)');
+
 					$this->tpl->assign_block_vars('bossprogress_cat.bossprogress_val', array(
 						'ID'	=> $v_bossprogress['id'],
 						'NAME'	=> $v_bossprogress['name'],
-						'BARS'	=> $this->html->ToolTip($tt_bossprogress , $this->html->bars(array(
-										array('value'	=> $v_bossprogress['bosses_normal'], 'max'	=> $v_bossprogress['bosses_max']),
-										array('value'	=> $v_bossprogress['bosses_heroic'], 'max'	=> $v_bossprogress['bosses_max'], 'text' => 'heroic')
-									),300), '', array('usediv'=>true, 'name'=>'tt_bossprogress')),
+						'BARS'	=> $this->html->ToolTip($tt_bossprogress , $bar_bc_normal.$bar_bc_heroic, '', array('usediv'=>true, 'name'=>'tt_bossprogress')),
 						'RUNS'	=> sprintf($this->game->glang('bossprogress_normalruns'), $v_bossprogress['runs_normal']).' '.sprintf($this->game->glang('bossprogress_heroicruns'), $v_bossprogress['runs_heroic'])
 					));
 				}
