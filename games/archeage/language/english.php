66b0d75c426390b2a2f1a983ef72a4b6a5849b5b
sionaa <sionaa@eqdkp-plus.eu>
2013-01-14 18:32:42 +0000

    add new game

---
+++ games/archeage/language/english.php
@@ -0,0 +1,71 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2010
+ * Date:		$Date: 2013-01-14 15:10:51 +0100 (Mo, 14 Jan 2013) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: sionaa $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 12841 $
+ * 
+ * $Id: english.php 12841 2013-01-14 14:10:51Z sionaa $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+$english_array = array(
+	'classes' => array(
+		0	=> 'Unknown',
+		1	=> 'Tank',
+		2	=> 'Healer',
+		3	=> 'Supporter',
+		4	=> 'Melee',
+		5	=> 'Ranged',
+
+	),
+
+	'races' => array(
+		'Unknown',
+		'Nuian',
+		'Elf',
+		'Hariharan',
+		'Ferre',
+
+	),
+
+	'lang' => array(
+		'archeage' => 'Archeage',
+		
+		// Profile information
+		'uc_gender'						=> 'Gender',
+		'uc_male'						=> 'Male',
+		'uc_female'						=> 'Female',
+		'uc_guild'						=> 'Guild',
+		'uc_ability_1'				=> '1. Ability',
+		'uc_ability_2'				=> '2. Ability',
+		'uc_ability_3'				=> '3. Ability',
+		
+		
+		// Advanced Class Information
+		
+		'uc_ab_0' => '-',
+		'uc_ab_1' => 'Combat',
+		'uc_ab_2' => 'Conjury',
+		'uc_ab_3' => 'Fortification',
+		'uc_ab_4' => 'Will',
+		'uc_ab_5' => 'Death',
+		'uc_ab_6' => 'Wild',
+		'uc_ab_7' => 'Magic',
+		'uc_ab_8' => 'Assassination',
+		'uc_ab_9' => 'Artistry',
+		'uc_ab_10' => 'Devotion',
+
+	),
+);
+?>
