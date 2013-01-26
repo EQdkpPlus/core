9aeb9a71bc1c578cbb667324beea336c3a87ffcf
godmod <godmod@eqdkp-plus.eu>
2013-01-26 13:03:04 +0000

    set external version to RC3

--- core/constants.php
+++ core/constants.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
  * -------------------------------------------------------------------------
  */
 define('VERSION_INT',		'1.0.9');				// the internal version number for updates & update check
-define('VERSION_EXT',		'1.0 RC 2');			// the external version number to be shown in the footer
+define('VERSION_EXT',		'1.0 RC 3');			// the external version number to be shown in the footer
 define('VERSION_WIP',		false);					// work in progress or stable?
 define('VERSION_PHP_RQ',	'5.2.0');				// required version of PHP
 define('VERSION_PHP_REC', 	'5.3.0');				// recommended version of PHP
