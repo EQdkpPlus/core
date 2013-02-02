7a6e23af7a248966d15d55a55754891498dbc383
godmod <godmod@eqdkp-plus.eu>
2013-02-02 00:20:45 +0000

    Live-Update: check php version as requirement

--- language/german/lang_admin.php
+++ language/german/lang_admin.php
@@ -1486,6 +1486,7 @@ $lang = array(
 "liveupdate_diff_added" => "Hinzugefügt",
 "liveupdate_diff_removed" => "Entfernt",
 "liveupdate_diff_line" => "Zeile",
+"liveupdate_update_requirements_failed" => "Das Live-Update kann nicht durchgeführt werden, da bestimmte Voraussetzungen nicht erfüllt sind. Lade dir das Update aus unserem <a href=\"".EQDKP_REPO_URL."\">Repository</a> herunter.", 
 
 "pi_rating" => "Bewertung",
 "pi_category_1" => "Plugins",
