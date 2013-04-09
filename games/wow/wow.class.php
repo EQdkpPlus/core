7c7c0ff413ae2dfc99a38f60a7f2639a9944a540
sionaa <sionaa@eqdkp-plus.eu>
2013-04-09 15:28:11 +0000

    updated roster raid progression with throne of thunder

--- games/wow/wow.class.php
+++ games/wow/wow.class.php
@@ -58,7 +58,7 @@ if(!class_exists('wow')) {
 		protected $lang_file	= array();
 		protected $path			= '';
 		public $lang			= false;
-		public $version			= '5.1';
+		public $version			= '5.1.1';
 
 		public function __construct() {
 			parent::__construct();
