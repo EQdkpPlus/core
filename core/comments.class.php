5ade4f2a170bc6941be828b318763ea178a2f639
wallenium <wallenium@eqdkp-plus.eu>
2013-07-25 10:44:25 +0000

    - grey delete icon for comments

--- core/comments.class.php
+++ core/comments.class.php
@@ -141,7 +141,7 @@ if (!class_exists("comments")){
 								<div class="comment_container">
 									<div class="comment_author"><a href="'.$this->routing->build('user', $row['username'], 'u'.$row['userid']).'">'.sanitize($row['username']).'</a> am '.$this->time->user_date($row['date'], true).'</div>';
 					if($this->isAdmin OR $row['userid'] == $this->UserID){
-						$out[] .= '<div class="comments_delete bold floatRight hand"><i class="icon-remove-sign icon-large"></i>';
+						$out[] .= '<div class="comments_delete bold floatRight hand"><i class="icon-remove-sign icon-large icon-grey"></i>';
 						$out[] .= '<div style="display:none" class="comments_page">'.$page.'</div>';
 						$out[] .= '<div style="display:none" class="comments_deleteid">'.$row['id'].'</div>';
 						$out[] .= '<div style="display:none" class="comments_attachid">'.$attachid.'</div>';
@@ -170,7 +170,7 @@ if (!class_exists("comments")){
 										<div class="comment_container">
 											<div class="comment_author"><a href="'.$this->routing->build('user', $com['username'], 'u'.$com['userid']).'">'.sanitize($com['username']).'</a> am '.$this->time->user_date($com['date'], true).'</div>';
 							if($this->isAdmin OR $com['userid'] == $this->UserID){
-								$out[] .= '<div class="comments_delete bold floatRight hand"><i class="icon-remove icon-large"></i>';
+								$out[] .= '<div class="comments_delete bold floatRight hand"><i class="icon-remove icon-large icon-grey"></i>';
 								$out[] .= '<div style="display:none" class="comments_page">'.$page.'</div>';
 								$out[] .= '<div style="display:none" class="comments_deleteid">'.$com['id'].'</div>';
 								$out[] .= '<div style="display:none" class="comments_attachid">'.$attachid.'</div>';
