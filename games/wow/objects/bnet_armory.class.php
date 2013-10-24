0f9f0a72771bfefe952ad57090ee2c86ad0d16b3
wallenium <wallenium@eqdkp-plus.eu>
2013-10-24 18:00:59 +0000

    - reduce memory usage of bnet_armory.class.php by removing the unused data modules and adding a "load if need" routine

--- games/wow/objects/bnet_armory.class.php
+++ games/wow/objects/bnet_armory.class.php
@@ -264,14 +264,17 @@ class bnet_armory {
 	* 
 	* @param $user		Character Name
 	* @param $realm		Realm Name
-	* @param $force		Force the cache to update?
+	* @param $force		Force the cache to update
+	* @param $params	array, optional fields such as reputation, appearance, mounts, pets, hunterPets,petSlots, quests, pvp
 	* @return bol
 	*/
-	public function character($user, $realm, $force=false){
-		$realm	= $this->ConvertInput($this->cleanServername($realm));
-		$user	= $this->ConvertInput($user);
-		$wowurl	= $this->_config['apiUrl'].sprintf('wow/character/%s/%s?locale=%s&fields=guild,stats,feed,talents,items,reputation,titles,professions,appearance,mounts,pets,achievements,progression,pvp,quests,hunterPets,petSlots', $realm, $user, $this->_config['locale']);
-		$json	= $this->get_CachedData('chardata_'.$user.$realm, $force);		
+	public function character($user, $realm, $force=false, $params=array()){
+		$realm		= $this->ConvertInput($this->cleanServername($realm));
+		$user		= $this->ConvertInput($user);
+		$basicparam	= array('guild', 'stats', 'feed', 'talents', 'items', 'titles', 'professions', 'achievements', 'progression');
+		$usedparams = array_merge($basicparam, $params);
+		$wowurl		= $this->_config['apiUrl'].sprintf('wow/character/%s/%s?locale=%s&fields='.implode(',', $usedparams), $realm, $user, $this->_config['locale']);
+		$json		= $this->get_CachedData('chardata_'.$user.$realm, $force);		
 		if(!$json && ($this->chardataUpdates < $this->_config['maxChardataUpdates'])){
 			$json	= $this->read_url($wowurl);
 			$this->set_CachedData($json, 'chardata_'.$user.$realm);
