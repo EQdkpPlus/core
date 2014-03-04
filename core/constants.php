66cd7bf3ce11580ade775c4eb2d85756f689dcce
wallenium <wallenium@eqdkp-plus.eu>
2014-03-04 20:04:29 +0000

    - bump version to 1.0.5

--- core/constants.php
+++ core/constants.php
@@ -25,8 +25,8 @@ if ( !defined('EQDKP_INC') ){
  * CORE VERSIONS
  * -------------------------------------------------------------------------
  */
-define('VERSION_INT',		'1.0.21');				// the internal version number for updates & update check
-define('VERSION_EXT',		'1.0.4');				// the external version number to be shown in the footer
+define('VERSION_INT',		'1.0.22');				// the internal version number for updates & update check
+define('VERSION_EXT',		'1.0.5');				// the external version number to be shown in the footer
 define('VERSION_WIP',		false);					// work in progress or stable?
 define('VERSION_PHP_RQ',	'5.2.0');				// required version of PHP
 define('VERSION_PHP_REC', 	'5.3.0');				// recommended version of PHP
