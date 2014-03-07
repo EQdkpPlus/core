f7b0070f0bc8bc297938fdecb4c1e792bd2ab9de
godmod <godmod@eqdkp-plus.eu>
2014-03-07 23:28:00 +0000

    - Added HTMl Field for uploading Files - Added Filetree to select uploaded Files

---
+++ core/html/hfile.class.php
@@ -0,0 +1,144 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2013
+ * Date:		$Date: 2013-12-29 19:01:00 +0100 (So, 29 Dez 2013) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: hoofy_leon $
+ * @copyright	2006-2013 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 13843 $
+ * 
+ * $Id: htext.class.php 13843 2013-12-29 18:01:00Z hoofy_leon $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+include_once(registry::get_const('root_path').'core/html/html.aclass.php');
+
+/*
+ * available options
+ * name			(string) 	name of the textarea
+ * id			(string)	id of the field, defaults to a clean form of name if not set
+ * value		
+ * class		(string)	class for the field
+ * readonly		(boolean)	field readonly?
+ * size			(int)		size of the field
+ * js			(string)	extra js which shall be injected into the field
+ * spinner		(boolean)	make a spinner out of the field?
+ * disabled		(boolean)	disabled field
+ * autocomplete	(array)		if not empty: array containing the elements on which to autocomplete (not to use together with spinner)
+ * colorpicker	(boolean) 	apply a colorpicker to this field
+ */
+class hfile extends html {
+
+	protected static $type = 'file';
+	
+	public $name = '';
+	public $readonly = false;
+	public $class = 'input';
+	public $inptype = '';
+	
+	
+	protected $mimetypes = false;
+	protected $numerate = false;
+	protected $extensions = array();
+	private $out = '';
+	
+	public function _construct() {
+		$out = '<input type="'.self::$type.'" name="'.$this->name.'" ';
+		if(empty($this->id)) $this->id = $this->cleanid($this->name);
+		$out .= 'id="'.$this->id.'" ';
+				
+		if(isset($this->value)) $out .= 'value="'.$this->value.'" ';
+		if(!empty($this->class)) $out .= 'class="'.$this->class.'" ';
+		if(!empty($this->size)) $out .= 'size="'.$this->size.'" ';
+		if($this->readonly) $out .= 'readonly="readonly" ';
+		if(!empty($this->js)) $out.= $this->js.' ';
+		$this->out = $out.' />';
+	}
+	
+	public function _toString() {
+		return $this->out;
+	}
+	
+	public function inpval() {
+		$tempname		= $_FILES[$this->name]['tmp_name'];
+		$filename		= $_FILES[$this->name]['name'];
+		$filetype		= $_FILES[$this->name]['type'];
+		if ($tempname == '') return false;
+
+		
+		$fileEnding		= pathinfo($filename, PATHINFO_EXTENSION);
+		if ($this->mimetypes){
+			$mime = false;
+			if(function_exists('finfo_open') && function_exists('finfo_file') && function_exists('finfo_close')){
+				echo "finfo";
+				$finfo			= finfo_open(FILEINFO_MIME);
+				$mime			= finfo_file($finfo, $tempname);
+				finfo_close($finfo);
+				
+				$mime = array_shift(preg_split('/[; ]/', $mime));					
+				if (!in_array($mime, $this->mimetypes)) return false;
+			}elseif(function_exists('mime_content_type')){
+				echo "mime_content";
+				$mime			= mime_content_type( $tempname );
+				$mime = array_shift(preg_split('/[; ]/', $mime));
+				if (!in_array($mime, $this->mimetypes)) return false;
+			}else{
+				// try to get the extension... not really secure...			
+				if (!in_array($fileEnding, $this->extensions)) return false;
+			}			
+			
+		} else {
+			
+			if (!in_array($fileEnding, $this->extensions)) {
+				return false;
+			}
+		}
+		
+		
+		if($this->numerate){
+			//Do no overwrite existing files
+			$offset = 0;
+			$files = array();
+			$file = scandir($this->root_path.$this->folder);
+			foreach($file as $this_file) {
+				if( valid_folder($this_file) && !is_dir($this_file)) {
+					$files[] = $this_file;
+				}
+			}
+			
+			$pathinfo = pathinfo($filename);
+			$name = $pathinfo['filename'];
+			
+			$arrFiles = preg_grep('/^' . preg_quote($name, '/') . '.*\.' . preg_quote($fileEnding, '/') . '/', $files);
+			
+			foreach ($arrFiles as $strFile){
+				if (preg_match('/_[0-9]+\.' . preg_quote($pathinfo['extension'], '/') . '$/', $strFile)){
+					$strFile = str_replace('.' . $pathinfo['extension'], '', $strFile);
+					$intValue = intval(substr($strFile, (strrpos($strFile, '_') + 1)));
+					$offset = max($offset, $intValue);
+				}
+			}
+			
+			$filename = str_replace($name, $name . '_' . ++$offset, $filename);	
+		}		
+
+		if (isFilelinkInFolder(str_replace(registry::get_const('root_path'),"", $this->folder.$filename), str_replace(registry::get_const('root_path'),"", $this->folder))) {
+			$this->pfh->FileMove($tempname, $this->folder.$filename, true);
+		} else {
+			unlink($tempname);
+			return false;
+		}
+
+		return str_replace(registry::get_const('root_path'),"", $this->folder.$filename);		
+	}
+}
+?>
---
+++ core/html/hfiletree.class.php
@@ -0,0 +1,165 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2013
+ * Date:		$Date: 2013-12-29 19:01:00 +0100 (So, 29 Dez 2013) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: hoofy_leon $
+ * @copyright	2006-2013 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 13843 $
+ * 
+ * $Id: htext.class.php 13843 2013-12-29 18:01:00Z hoofy_leon $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+include_once(registry::get_const('root_path').'core/html/html.aclass.php');
+
+/*
+ * available options
+ * name			(string) 	name of the textarea
+ * id			(string)	id of the field, defaults to a clean form of name if not set
+ * value		
+ * class		(string)	class for the field
+ * readonly		(boolean)	field readonly?
+ * size			(int)		size of the field
+ * js			(string)	extra js which shall be injected into the field
+ * spinner		(boolean)	make a spinner out of the field?
+ * disabled		(boolean)	disabled field
+ * autocomplete	(array)		if not empty: array containing the elements on which to autocomplete (not to use together with spinner)
+ * colorpicker	(boolean) 	apply a colorpicker to this field
+ */
+class hfiletree extends html {
+
+	protected static $type = 'text';
+	
+	public $name = '';
+	public $folder;
+	public $extensions = array();
+	public $inputBoxFormat = 'radio';
+	public $dirsOnly = false;
+	
+
+	public $inptype = '';
+	
+	private $out = '';
+	
+	public function _construct() {
+		
+		
+		
+		$this->tpl->add_js('$(document).ready( function() {
+		
+			// Hide all sub$folders at startup
+			$(".file-tree-'.$this->name.'").find("UL").hide();
+		
+			// Expand/collapse on click
+			$(".file-tree-'.$this->name.' .pft-directory a").click( function() {
+				$(this).parent().find("UL:first").slideToggle("medium");
+				if( $(this).parent().attr(\'className\') == "pft-directory" ) return false;
+			});
+			$(".file-tree-'.$this->name.' .pft-root-directory a").click( function() {
+				console.log($(this).parent());
+				$(this).parent().parent().find("UL:first").slideToggle("medium");
+			});
+		});', 'docready');
+		
+		
+		$this->tpl->add_css('				
+			.pft-directory ul {
+				margin-left: 10px;
+			}
+				
+			.file-tree-'.$this->name.' > ul {
+				margin-left: 10px;
+			}
+				
+			.file-tree-'.$this->name.' li {
+				padding: 3px;
+			}
+		');
+
+		$this->out = "<ul class=\"file-tree-".$this->name."\"><li class=\"pft-root-directory\"><a href=\"javascript:void(0);\"><i class=\"fa fa-lg fa-folder\"></i> ".$this->folder.'</a></li>'.$this->file_tree($this->folder).'</ul>';
+		if (!$this->out) $this->out = "";
+	}
+	
+	public function _toString() {
+		return $this->out;
+	}
+	
+	public function inpval() {
+		return $this->in->get($this->name, '', $this->inptype);
+	}
+	
+	private function file_tree($folder, $first_call=true) {
+		if (!is_array($this->value)) $this->value = array($this->value);
+		// Get and sort directories/files
+		$file = scandir($folder);
+		natcasesort($file);
+	
+		// Make directories first
+		$files = $dirs = array();
+		foreach($file as $this_file) {
+			if( is_dir("$folder/$this_file" ) ) {
+				$dirs[] = $this_file;
+			} elseif (!$this->dirsOnly && valid_folder($this_file)){
+				$files[] = $this_file;
+			}
+		}
+		$file = array_merge($dirs, $files);
+	
+		// Filter unwanted extensions
+		if( !empty($this->extensions) ) {
+			foreach( array_keys($file) as $key ) {
+				if( !is_dir("$folder/$file[$key]") ) {
+					$ext = substr($file[$key], strrpos($file[$key], ".") + 1);
+					if( !in_array($ext, $this->extensions) ) unset($file[$key]);
+				}
+			}
+		}
+		$dd_data = array();
+	
+		if( count($file) > 2 ) { // Use 2 instead of 0 to account for . and .. "directories"
+			$php_file_tree = "<ul";
+			//if( $first_call ) { $php_file_tree .= " class=\"file-tree-".$this->name."\""; $first_call = false; }
+			$php_file_tree .= ">";
+				
+			foreach( $file as $this_file ) {
+				if( $this_file != "." && $this_file != ".." ) {
+					if( is_dir("$folder/$this_file") ) {
+						// Directory
+						$php_file_tree .= "<li class=\"pft-directory\"><i class=\"fa fa-lg fa-folder-o\"></i> ".(($this->inputBoxFormat == "checkbox") ? "<input type=\"checkbox\" name=\"".$this->name."[]\" value=\"".str_replace("//", "/", $folder."/".$this_file)."\"> " : '')."<a href=\"javascript:void(0);\">" . sanitize($this_file) . "</a>";
+						$php_file_tree .= $this->file_tree("$folder/$this_file", false);
+						$php_file_tree .= "</li>";
+						$dd_data["$folder/$this_file"] = $this_file;
+						$bla = $this->file_tree(str_replace("//", "/", $folder."/".$this_file), false);
+	
+						if (is_array($bla)){
+							foreach ($bla as $key => $value){
+								$dd_data[$key] = '&nbsp;&nbsp;&nbsp;'. $value;
+							}
+						}
+						$i++;
+					} else {
+						// File
+						// Get extension (prepend 'ext-' to prevent invalid classes from extensions that begin with numbers)
+						$ext = "ext-" . substr($this_file, strrpos($this_file, ".") + 1);
+						$link = str_replace("[link]", "$folder/" . urlencode($this_file), $return_link);
+						$php_file_tree .= "<li class=\"pft-file " . strtolower($ext) . "\">".(($this->inputBoxFormat == "checkbox") ? '<input type="checkbox" name="'.$this->name.'[]" value="'.str_replace("//", "/", $folder."/".$this_file).'"> ': '').((true) ? "<input type=\"radio\" name=\"".$this->name."\" value=\"".str_replace("//", "/", $folder."/".$this_file)."\"".((in_array(str_replace("//", "/", $folder."/".$this_file), $this->value)) ? ' checked="checked"' : '')."> " : '')."<a>" . sanitize($this_file) . "</a></li>";
+					}
+				}
+			}
+			$php_file_tree .= "</ul>";
+		}
+		
+		return $php_file_tree;
+	}
+}
+?>
