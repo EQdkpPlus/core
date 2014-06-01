1cb7172e32dc064b7b93a0bd4378d1b0aa4c3983
godmod <godmod@eqdkp-plus.eu>
2014-06-01 16:20:45 +0000

    fixed xenforo bridge

--- core/bridges/xenforo.bridge.class.php
+++ core/bridges/xenforo.bridge.class.php
@@ -48,7 +48,6 @@ class xenforo_bridge extends bridge_generic {
 			'email'	=> 'email',
 			'salt'	=> '',
 			'QUERY'	=> '',
-			'FUNCTION' => 'get_user',
 		),
 	);
 	
@@ -69,11 +68,9 @@ class xenforo_bridge extends bridge_generic {
 	//Needed function
 	public function check_password($password, $hash, $strSalt = '', $boolUseHash, $strUsername){
 		$query = $this->db->query("SELECT * FROM ".$this->prefix."user_authenticate WHERE user_id=".$this->db->escape(intval($hash)));
-		$result = $this->db->fetch_row($query);
 		while($arrResult = $this->db->fetch_record($query)){
 			$arrAuthData = unserialize($arrResult['data']);
 			$scheme = $arrResult['scheme_class'];
-				
 			$blnResult = $this->_handle_logins($scheme, $password, $hash, $arrAuthData);
 			if ($blnResult) return true;
 		}
@@ -83,6 +80,10 @@ class xenforo_bridge extends bridge_generic {
 
 	
 	private function _handle_logins($scheme, $password, $hash, $arrAuthData){
+		d($scheme);
+		d($password);
+		d($hash);
+		d($arrAuthData);
 		switch($scheme){
 			case "XenForo_Authentication_Core12":
 				$passwordHash = new XenForo_PasswordHash($this->passwordIterations, false);
