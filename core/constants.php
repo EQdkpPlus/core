75abf173541efb0615b29c1690ef71b67df51179
wallenium <wallenium@eqdkp-plus.eu>
2014-06-17 13:40:39 +0000

    - update for alpha 1

--- core/constants.php
+++ core/constants.php
@@ -26,12 +26,12 @@ if ( !defined('EQDKP_INC') ){
  * -------------------------------------------------------------------------
  */
 define('VERSION_INT',		'2.0.0.8');				// the internal version number for updates & update check
-define('VERSION_EXT',		'2.0.0 Alpha');			// the external version number to be shown in the footer
+define('VERSION_EXT',		'2.0.0 Alpha 1');		// the external version number to be shown in the footer
 define('VERSION_WIP',		true);					// work in progress or stable?
 define('VERSION_PHP_RQ',	'5.4.0');				// required version of PHP
 define('VERSION_PHP_REC', 	'5.5.0');				// recommended version of PHP
 define('REQ_PHP_MEMORY', 	'60M');					// required PHP Memory
-define('REQ_PHP_MEMORY_REC', 	'128M');					// required PHP Memory
+define('REQ_PHP_MEMORY_REC', 	'128M');			// required PHP Memory
 
 // Plugin states
 define('PLUGIN_INSTALLED',		 1);
