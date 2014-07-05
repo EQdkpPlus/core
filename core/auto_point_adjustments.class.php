5464106197db3b83fe1ad3f888bcedb2988f8fb2
hoofy_leon <hoofy_leon@eqdkp-plus.eu>
2014-07-05 11:37:07 +0000

    Fix APA RIA applies decay immediatley #817 Fix and enable cache for APAs

--- core/auto_point_adjustments.class.php
+++ core/auto_point_adjustments.class.php
@@ -21,7 +21,7 @@ if ( !defined('EQDKP_INC') ){
 }
 	class auto_point_adjustments extends gen_class {
 		public static $shortcuts = array('pfh', 'pdc', 'time', 'user');
-		public static $dependencies = array('pdc');
+		public static $dependencies = array('pdc', 'pfh', 'pdl');
 
 		private $apa_tab			= array();
 		private $apa_tab_file		= 'apatab.php';
@@ -334,14 +334,14 @@ if ( !defined('EQDKP_INC') ){
 		}
 
 		public function __destruct() {
-			#foreach($this->ttls as $apa_id => $data) {
-			#	foreach($data as $cache_date => $ttl) {
-			#		$this->pdc->put('apa_'.$apa_id.'_'.$cache_date, $this->cached_data[$apa_id][$cache_date], $ttl);
-			#	}
-			#}
-			#$this->pdc->put('apa_update_table', $this->needs_update, 15768000); //cache half a year
-			#unset($this->cached_data);
-			#unset($this->ttls);
+			foreach($this->ttls as $apa_id => $data) {
+				foreach($data as $cache_date => $ttl) {
+					$this->pdc->put('apa_'.$apa_id.'_'.$cache_date, $this->cached_data[$apa_id][$cache_date], $ttl);
+				}
+			}
+			$this->pdc->put('apa_update_table', $this->needs_update, 15768000); //cache half a year
+			unset($this->cached_data);
+			unset($this->ttls);
 			parent::__destruct();
 		}
 	}//end class
