1f78fa8da63eab9ee6c3e4c8ffc68820bc5d13ff
God Mod <godmod23@web.de>
2015-05-07 13:36:54 +0200

    updated tinymce to 4.1.10

--- libraries/tinyMCE/tinyMCE.class.php
+++ libraries/tinyMCE/tinyMCE.class.php
@@ -25,7 +25,7 @@ if ( !defined('EQDKP_INC') ){
 
 class tinyMCE extends gen_class {
 
-	protected $tinymce_version = '4.1.7';
+	protected $tinymce_version = '4.1.10';
 	protected $language	= 'en';
 	protected $trigger	= array(
 		'bbcode'	=> false,
