4a37cfe95af8b6f0acf17d3e5504db11a22b728f
God Mod <godmod23@web.de>
2015-08-15 17:14:40 +0200

    #1309

--- core/bridges/mybb.bridge.class.php
+++ core/bridges/mybb.bridge.class.php
@@ -129,7 +129,7 @@ class mybb_bridge extends bridge_generic {
 	private function _handle_birthday($date){
 		list($d, $m, $y) = explode('-', $date);
 		if ($y != '' && $y != 0 && $m != '' && $m != 0 && $d != '' && $d != 0){
-			return $this->time->mktime(0,0,0,$m,$d,$y);
+			return mktime(2, 1, 0, $m, $d, $y);
 		}
 		return 0;
 	}
