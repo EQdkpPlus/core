0b84f2f6ddd2ce2dad781b88c37f7938a260cdfb
God Mod <godmod23@web.de>
2015-11-09 16:39:15 +0100

    #1374

--- admin/manage_settings.php
+++ admin/manage_settings.php
@@ -522,7 +522,8 @@ class mmocms_settings extends page_generic {
 						'options'		=> $members,
 					),
 					'show_twinks'	=> array(
-						'type'		=> 'radio'
+						'type'		=> 'radio',
+						'dependency'=> array(0 => array('detail_twink')),
 					),
 					'detail_twink'	=> array(
 						'type'		=> 'radio'
