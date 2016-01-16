066b7681ba1d2fd5f4632c1c5b45b41e3e32a070
Simon W <s.wallmann@kompsoft.de>
2016-01-16 17:44:28 +0100

    #1414

--- core/pageobjects/calendarevent_pageobject.class.php
+++ core/pageobjects/calendarevent_pageobject.class.php
@@ -789,7 +789,7 @@ class calendarevent_pageobject extends pageobject {
 			}
 		}
 
-		$this->tpl->add_js("var roles_json = ".json_encode($drpdwn_roles));
+		$this->tpl->add_js("var roles_json = ".json_encode($drpdwn_roles).";", 'head');
 
 		// Dropdown Menu Array
 		$nextraidevent	= $this->pdh->get('calendar_events', 'next_raid', array($this->url_id));
