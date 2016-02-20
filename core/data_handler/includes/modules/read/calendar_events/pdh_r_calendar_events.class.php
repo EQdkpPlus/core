b63e21cff49e9b3eac9e13a3124bda9f98a9d8d6
Simon W <s.wallmann@kompsoft.de>
2016-02-20 13:09:57 +0100

    #1459

--- core/data_handler/includes/modules/read/calendar_events/pdh_r_calendar_events.class.php
+++ core/data_handler/includes/modules/read/calendar_events/pdh_r_calendar_events.class.php
@@ -406,7 +406,18 @@ if ( !class_exists( "pdh_r_calendar_events" ) ) {
 			return $this->pdh->geth('event', 'icon', array($this->events[$id]['extension']['raid_eventid'])).' '.$raideventname;
 		}
 
-	public function get_next_event($id){
+		// check calendar specific rights such as if the user is a raidleader or the creator
+		public function get_check_operatorperm($raidid, $userid=0){
+			$userid	= ($userid > 0) ? $userid : $this->user->data['user_id'];
+			$creator			= $this->pdh->get('calendar_events', 'creatorid', array($raidid));
+			$ev_ext				= $this->pdh->get('calendar_events', 'extension', array($raidid));
+			$raidleaders_chars	= ($ev_ext['raidleader'] > 0) ? $ev_ext['raidleader'] : array();
+			$raidleaders_users	= $this->pdh->get('member', 'userid', array($raidleaders_chars));
+			if (!is_array($raidleaders_users)) $raidleaders_users = array();
+			return (($creator == $userid) || in_array($userid, $raidleaders_users))  ? true : false;
+		}
+
+		public function get_next_event($id){
 			try {
 				$this->helper_set_pointer($this->event_timestamps, $id);
 			}catch(Exception $e){
