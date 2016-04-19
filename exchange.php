27ec50ccb240a574411cfd3793257703e597b2b0
Simon W <s.wallmann@kompsoft.de>
2016-04-19 14:15:00 +0200

    fixed a small issue on ical feed

--- exchange.php
+++ exchange.php
@@ -164,7 +164,7 @@ if(registry::register('input')->get('out') != ''){
 								$e->setProperty('dtstart',		array("timestamp" => registry::register('plus_datahandler')->get('calendar_events', 'time_start', array($calid)).'Z'));
 								$e->setProperty('dtend',		array("timestamp" => registry::register('plus_datahandler')->get('calendar_events', 'time_end', array($calid)).'Z'));
 								$e->setProperty('summary',		registry::register('plus_datahandler')->get('calendar_events', 'name', array($calid)));
-								$e->setProperty('description',	$description_data);
+								$e->setProperty('description',	rtrim($description_data));
 								$e->setProperty('class',		'PUBLIC');
 								$e->setProperty('categories',	'PERSONAL');
 								$v->setComponent($e);
