375eb3a3592604bc78026eeaef9f08b5ef6d74e1
God Mod <godmod23@web.de>
2016-08-08 18:07:47 +0200

    TinymCE Update

--- libraries/tinyMCE/tinyMCE.class.php
+++ libraries/tinyMCE/tinyMCE.class.php
@@ -25,7 +25,7 @@ if ( !defined('EQDKP_INC') ){
 
 class tinyMCE extends gen_class {
 
-	protected $tinymce_version = '4.3.3';
+	protected $tinymce_version = '4.4.1';
 	protected $language	= 'en';
 	protected $trigger	= array(
 		'bbcode'	=> false,
