0a02db9612665fb44c269f2c3d4a3fbbbd99de68
Simon W <s.wallmann@kompsoft.de>
2016-10-20 14:04:51 +0200

    bugfix, same error, other location

--- admin/manage_calendars.php
+++ admin/manage_calendars.php
@@ -127,7 +127,7 @@ class Manage_Calendars extends page_generic {
 			'TYPE'				=> new hdropdown('calendars['.$key.'][type]', array('options' => $types, 'value' => $this->pdh->get('calendars', 'type', array($id)), 'id' => 'calendars'.$key)),
 			'COLOR'				=> $this->jquery->colorpicker('cal_'.$key, '', 'calendars['.$key.'][color]'),
 			'CALENDAR_COUNT'	=> $ranks,
-			'PERMISSIONS'		=> new hmultiselect('calendars['.$key.'][permissions]', array('options' => $this->pdh->aget('user_groups', 'name', 0, array($this->pdh->get('user_groups', 'id_list'))), 'value' => (($permissionvalue = 'all') ? $this->pdh->get('user_groups', 'id_list')) : $permissionvalue))),
+			'PERMISSIONS'		=> new hmultiselect('calendars['.$key.'][permissions]', array('options' => $this->pdh->aget('user_groups', 'name', 0, array($this->pdh->get('user_groups', 'id_list'))), 'value' => (($permissionvalue = 'all') ? $this->pdh->get('user_groups', 'id_list') : $permissionvalue))),
 		));
 
 		$this->core->set_vars(array(
