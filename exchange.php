1f40d99995e1ce4e4311c04d6a65012a3711d395
God Mod <godmod23@web.de>
2016-12-12 16:58:58 +0100

    Prevent notice

--- exchange.php
+++ exchange.php
@@ -25,7 +25,7 @@ define('NO_MMODE_REDIRECT', true);
 
 //Create no session for external portal modules.
 //Using Get here is neccessary, because input class not loaded yet and not working with the user input
-if($_GET['out'] == 'portal'){
+if(isset($_GET['out']) && $_GET['out'] == 'portal'){
 	define('NO_SESSION', true);
 }
 
