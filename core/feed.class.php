6833d445af13f498c59b4f808a8344b6b45faaf6
wallenium <s.wallmann@kompsoft.de>
2017-01-13 12:13:27 +0100

    small change

--- core/feed.class.php
+++ core/feed.class.php
@@ -91,9 +91,9 @@ if (!class_exists("feed")) {
 
 		private function specialchars($strString, $noHTML=false){
 			// encode URL properly
-			if(filter_var($sanitized_txt, FILTER_VALIDATE_URL)){
+			if(filter_var($strString, FILTER_VALIDATE_URL)){
 				$parsed_url	= parse_url($sanitized_txt);
-				return 'debug:'.$parsed_url['scheme'].'://'.$parsed_url['host'].urlrawencode($parsed_url['path'].$parsed_url['query']);
+				return $parsed_url['scheme'].'://'.$parsed_url['host'].urlrawencode($parsed_url['path'].$parsed_url['query']);
 				#return urlencode($sanitized_txt);
 			}
 			$arrFind		= array('"', "'", '<', '>', '&');
