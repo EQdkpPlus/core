e58c0a37e4b724e460ce2f533f03eef3feb0d0b3
GodMod <godmod23@web.de>
2017-03-07 19:47:26 +0100

    Some fixes

--- core/exchange/notifications.php
+++ core/exchange/notifications.php
@@ -1,23 +1,23 @@
 <?php
 /*	Project:	EQdkp-Plus
  *	Package:	EQdkp-plus
- *	Link:		http://eqdkp-plus.eu
- *
- *	Copyright (C) 2006-2016 EQdkp-Plus Developer Team
- *
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Affero General Public License as published
- *	by the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Affero General Public License for more details.
- *
- *	You should have received a copy of the GNU Affero General Public License
- *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*	Link:		http://eqdkp-plus.eu
+*
+*	Copyright (C) 2006-2016 EQdkp-Plus Developer Team
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU Affero General Public License as published
+*	by the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU Affero General Public License for more details.
+*
+*	You should have received a copy of the GNU Affero General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 if (!defined('EQDKP_INC')){
 	die('Do not access this file directly.');
@@ -30,45 +30,45 @@ if (!class_exists('exchange_notifications')){
 		public function get_notifications($params, $body){
 			if($this->user->is_signedin()){
 				$intUserID = $this->user->id;
-				
+
 				$arrNotifications = $this->ntfy->getAllUserNotifications();
-				
+
 				$intNumber = (intval($params['get']['number']) > 0) ?  intval($params['get']['number']) : 50;
 				$intStart = (intval($params['get']['start']) > 0) ?  intval($params['get']['start']) : 0;
-				
+
 				$intTotalCount = count($arrNotifications);
 				$arrNotifications = $this->pdh->limit($arrNotifications, $intStart, $intNumber);
-				
+
 				$arrOut = array();
-				
+
 				foreach($arrNotifications as $arrNotification){
 					if (isset($arrNotification['persistent'])) {
 						$arrOut[] = array(
-							'persistent' => 1,
-							'name'		=> $arrNotification['name'],
-							'prio'		=> $arrNotification['prio'],
-							'icon'		=> ($arrNotification['icon'] != "") ? $this->core->icon_font($arrNotification['icon']).' ' : '',
-							'class' 	=> 'unread',
-							'link'		=> $arrNotification['link'],
-							'time'		=> 0,
-							'read'		=> 0,
-							'id'		=> 0,
+								'persistent' => 1,
+								'name'		=> $arrNotification['name'],
+								'prio'		=> $arrNotification['prio'],
+								'icon'		=> ($arrNotification['icon'] != "") ? $this->core->icon_font($arrNotification['icon']).' ' : '',
+								'class' 	=> 'unread',
+								'link'		=> $arrNotification['link'],
+								'time'		=> 0,
+								'read'		=> 0,
+								'id'		=> 0,
 						);
 					} else {
 						$arrOut[] = array(
-							'persistent' => 0,
-							'name'		=> $arrNotification['name'],
-							'prio'		=> $arrNotification['prio'],
-							'icon'		=> ($arrNotification['icon'] != "") ? $this->core->icon_font($arrNotification['icon']).' ' : '',
-							'class' 	=> ($arrNotification['read']) ? 'read' : 'unread',
-							'link'		=> $arrNotification['link'],
-							'time'		=> (!$arrNotification['read']) ? $this->env->buildlink().$this->routing->build('Notifications', false, false, false, true).'&redirect='.$arrNotification['id'] : $this->env->link.$this->controller_path_plain.$arrNotification['link'],
-							'read'		=> ($arrNotification['read']) ? true : false,
-							'id'		=> $arrNotification['id'],
+								'persistent' => 0,
+								'name'		=> $arrNotification['name'],
+								'prio'		=> $arrNotification['prio'],
+								'icon'		=> ($arrNotification['icon'] != "") ? $this->core->icon_font($arrNotification['icon']).' ' : '',
+								'class' 	=> ($arrNotification['read']) ? 'read' : 'unread',
+								'time'		=> $arrNotification['time'],
+								'link'		=> (!$arrNotification['read']) ? $this->env->buildlink().$this->routing->build('Notifications', false, false, false, true).'&redirect='.$arrNotification['id'] : $this->env->link.$arrNotification['link'],
+								'read'		=> ($arrNotification['read']) ? true : false,
+								'id'		=> $arrNotification['id'],
 						);
-					}					
+					}
 				}
-				
+
 				return $arrOut;
 			} else {
 				return $this->pex->error('access denied');
