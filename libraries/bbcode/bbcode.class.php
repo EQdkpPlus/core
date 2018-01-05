e46d8cacd5ecd0a5fd475bb552b3fb4e9e2103a9
GodMod <godmod23@web.de>
2018-01-05 18:40:13 +0100

    Revert "removed tags that dont make sense"
    This reverts commit 85aa88c8fa121cb4f81ac7afc80c32f29d2c2759.

--- libraries/bbcode/bbcode.class.php
+++ libraries/bbcode/bbcode.class.php
@@ -314,11 +314,21 @@ if (!class_exists("bbcode")) {
 				switch (strtolower($elements[0])) {
 				
 					case 'article_url':
+						$strPath = $this->controller_path.$this->pdh->get('articles', 'path', array($elements[1]));
+						$arrCache[$strTag] = ($strPath) ? $strPath : '';
+						break;
+						
+					case 'article_url_plain':
 						$strPath = $this->controller_path_plain.$this->pdh->get('articles', 'path', array($elements[1]));
 						$arrCache[$strTag] = ($strPath) ? $strPath : '';
 						break;
 						
 					case 'category_url':
+						$strPath = $this->controller_path.$this->pdh->get('article_categories', 'path', array($elements[1]));
+						$arrCache[$strTag] = ($strPath) ? $strPath : '';
+						break;
+						
+					case 'category_url_plain':
 						$strPath = $this->controller_path_plain.$this->pdh->get('article_categories', 'path', array($elements[1]));
 						$arrCache[$strTag] = ($strPath) ? $strPath : '';
 						break;
