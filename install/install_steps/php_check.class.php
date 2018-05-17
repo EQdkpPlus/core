ce75de6603112514e99f81ab3de49a6e2731b905
GodMod <godmod23@web.de>
2018-05-17 19:55:24 +0200

    Fixed pathinfo check

--- install/install_steps/php_check.class.php
+++ install/install_steps/php_check.class.php
@@ -104,19 +104,19 @@ class php_check extends install_generic {
 				'adviced_fail'	=> (!function_exists('mb_strtolower')) ? true : false,
 				'ignore'		=> true
 			),
-			//Check will be performed by javascript
-			'pathinfo'	=> array(
-				'required'		=> $this->lang['yes'],
-				'installed'		=> $this->lang['yes'],
-				'passfail'		=> true
-			),
 			'externalconnection' => array(
 				'required'		=> $this->lang['yes'],
 				'installed'		=> ($this->check_external_connection()) ? $this->lang['yes']: $this->lang['no'],
 				'ignore'		=> true,
 				'passfail'		=> true,
 				'adviced_fail'	=> (!$this->check_external_connection()),
-			)
+			),
+			//Check will be performed by javascript; fixed to last row in javascript
+			'pathinfo'	=> array(
+					'required'		=> $this->lang['yes'],
+					'installed'		=> $this->lang['yes'],
+					'passfail'		=> true
+			),
 		);
 	}
 	
