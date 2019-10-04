1843f92faa3067a1056eadacec634e2207e97c61
GodMod <godmod23@web.de>
2019-10-04 13:34:21 +0200

    Allow normal authorization header

--- core/plus_exchange.class.php
+++ core/plus_exchange.class.php
@@ -244,27 +244,53 @@ if( !class_exists( "plus_exchange" ) ) {
 					return $intUserID;
 				}
 				
-			} elseif(isset($_SERVER['HTTP_X_CUSTOM_AUTHORIZATION']) && strlen($_SERVER['HTTP_X_CUSTOM_AUTHORIZATION'])){
-				$arrToken = array();
-				parse_str($_SERVER['HTTP_X_CUSTOM_AUTHORIZATION'], $arrToken);
-				$strToken = isset($arrToken['token']) ? $arrToken['token'] : "";
-				$strTokenType = isset($arrToken['type']) ? $arrToken['type'] : "";
-				
-				if(strlen($strTokenType) && strlen($strToken)){
-					if($strTokenType === 'api'){
-						if(strlen($strToken) && $strToken === $this->config->get('api_key')){
-							$this->isCoreAPIToken = true;
-						}
-					} else {
-						$intUserID = $this->user->getUserIDfromDerivedExchangekey($strToken, 'pex_api');
-						$this->user->changeSessionUser($intUserID);
-						return $intUserID;
-					}
+			} else {
+				$headers = $this->getAuthorizationHeader();
+				if($headers){
+					$arrToken = array();
+					parse_str($headers, $arrToken);
+					$strToken = isset($arrToken['token']) ? $arrToken['token'] : "";
+					$strTokenType = isset($arrToken['type']) ? $arrToken['type'] : "";
 					
+					if(strlen($strTokenType) && strlen($strToken)){
+						if($strTokenType === 'api'){
+							if(strlen($strToken) && $strToken === $this->config->get('api_key')){
+								$this->isCoreAPIToken = true;
+							}
+						} else {
+							$intUserID = $this->user->getUserIDfromDerivedExchangekey($strToken, 'pex_api');
+							$this->user->changeSessionUser($intUserID);
+							return $intUserID;
+						}
+						
+					}
 				}
 			}
 
 			return $this->user->id;
+		}
+		
+		private function getAuthorizationHeader(){
+			$headers = null;
+			if (isset($_SERVER['Authorization'])) {
+				$headers = trim($_SERVER["Authorization"]);
+			}elseif (isset($_SERVER['HTTP_X_CUSTOM_AUTHORIZATION'])){
+				$headers = trim($_SERVER['HTTP_X_CUSTOM_AUTHORIZATION']);
+			}
+			else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+				$headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+			} elseif (function_exists('apache_request_headers')) {
+				$requestHeaders = apache_request_headers();
+				// Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+				$requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+				//print_r($requestHeaders);
+				if (isset($requestHeaders['Authorization'])) {
+					$headers = trim($requestHeaders['Authorization']);
+				} elseif(isset($requestHeaders['HTTP_X_CUSTOM_AUTHORIZATION'])) {
+					$headers = trim($requestHeaders['HTTP_X_CUSTOM_AUTHORIZATION']);
+				}
+			}
+			return $headers;
 		}
 		
 		public function getIsApiTokenRequest(){
