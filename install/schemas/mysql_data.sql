3709b3f45e194070b5204bfaf7742c71f6574420
GodMod <godmod23@web.de>
2019-11-03 18:31:10 +0100

    Changed link structure on install

--- install/schemas/mysql_data.sql
+++ install/schemas/mysql_data.sql
@@ -76,11 +76,8 @@ INSERT INTO __member_ranks (`rank_id`, `rank_name`) VALUES ('0', '');
 INSERT INTO __member_ranks (`rank_id`, `rank_name`) VALUES ('1', 'Member');
 
 ## Links
-INSERT INTO `__links` (`link_id`, `link_url`, `link_name`, `link_window`, `link_menu`, `link_sortid`, `link_visibility`, `link_height`) VALUES (1, '#', 'Guild', '0', 0, 0, '[&#34;0&#34;]', 4024);
 INSERT INTO `__links` (`link_id`, `link_url`, `link_name`, `link_window`, `link_menu`, `link_sortid`, `link_visibility`, `link_height`) VALUES (2, '#', 'Links', '0', 0, 0, '[&#34;0&#34;]', 4024);
 INSERT INTO `__links` (`link_id`, `link_url`, `link_name`, `link_window`, `link_menu`, `link_sortid`, `link_visibility`, `link_height`) VALUES (3, 'http://eqdkp-plus.eu', 'EQdkp-Plus', '1', 0, 0, '[&#34;0&#34;]', 4024);
-INSERT INTO `__links` (`link_id`, `link_url`, `link_name`, `link_window`, `link_menu`, `link_sortid`, `link_visibility`, `link_height`) VALUES (4, '#', 'DKP-System', '0', 0, 0, '[&#34;0&#34;]', 4024);
-
 
 #multidkp-pool
 INSERT INTO __multidkp (`multidkp_id`, `multidkp_name`, `multidkp_desc`) VALUES ('1', 'Default', 'Default-Pool');
