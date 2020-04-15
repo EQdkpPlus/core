18d0d8233284e52225ee3db10a035bf1634c6f4b
GodMod <godmod23@web.de>
2020-04-15 18:36:26 +0200

    Changes in Browser detection

--- core/environment.class.php
+++ core/environment.class.php
@@ -82,18 +82,20 @@ if (!class_exists("environment")) {
 		*/
 		public $arrBrowser = array
 		(
-				'MSIE'       => array('browser'=>'ie',           'shorty'=>'ie', 'engine'=>'trident', 'version'=>'/^.*?MSIE (\d+(\.\d+)*).*$/'),
-				'Firefox'    => array('browser'=>'firefox',      'shorty'=>'fx', 'engine'=>'gecko',   'version'=>'/^.*Firefox\/(\d+(\.\d+)*).*$/'),
-				'Chrome'     => array('browser'=>'chrome',       'shorty'=>'ch', 'engine'=>'webkit',  'version'=>'/^.*Chrome\/(\d+(\.\d+)*).*$/'),
-				'OmniWeb'    => array('browser'=>'omniweb',      'shorty'=>'ow', 'engine'=>'webkit',  'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
-				'Silk'       => array('browser'=>'silk',         'shorty'=>'si', 'engine'=>'silk',    'version'=>'/^.*Silk\/(\d+(\.\d+)*).*$/'),
-				'Safari'     => array('browser'=>'safari',       'shorty'=>'sf', 'engine'=>'webkit',  'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
-				'Opera Mini' => array('browser'=>'opera-mini',   'shorty'=>'oi', 'engine'=>'presto',  'version'=>'/^.*Opera Mini\/(\d+(\.\d+)*).*$/'),
-				'Opera Mobi' => array('browser'=>'opera-mobile', 'shorty'=>'om', 'engine'=>'presto',  'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
-				'Opera'      => array('browser'=>'opera',        'shorty'=>'op', 'engine'=>'presto',  'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
-				'IEMobile'   => array('browser'=>'ie-mobile',    'shorty'=>'im', 'engine'=>'trident', 'version'=>'/^.*IEMobile (\d+(\.\d+)*).*$/'),
-				'Camino'     => array('browser'=>'camino',       'shorty'=>'ca', 'engine'=>'gecko',   'version'=>'/^.*Camino\/(\d+(\.\d+)*).*$/'),
-				'Konqueror'  => array('browser'=>'konqueror',    'shorty'=>'ko', 'engine'=>'webkit',  'version'=>'/^.*Konqueror\/(\d+(\.\d+)*).*$/')
+				'MSIE'       => array('browser'=>'ie',           'shorty'=>'ie', 'engine'=>'trident',  'version'=>'/^.*MSIE (\d+(\.\d+)*).*$/'),
+				'Trident'    => array('browser'=>'ie',           'shorty'=>'ie', 'engine'=>'trident',  'version'=>'/^.*Trident\/\d+\.\d+; rv:(\d+(\.\d+)*).*$/'),
+				'Edge'       => array('browser'=>'edge',         'shorty'=>'ed', 'engine'=>'edgehtml', 'version'=>'/^.*Edge\/(\d+(\.\d+)*).*$/'),
+				'Firefox'    => array('browser'=>'firefox',      'shorty'=>'fx', 'engine'=>'gecko',    'version'=>'/^.*Firefox\/(\d+(\.\d+)*).*$/'),
+				'Chrome'     => array('browser'=>'chrome',       'shorty'=>'ch', 'engine'=>'blink',    'version'=>'/^.*Chrome\/(\d+(\.\d+)*).*$/'),
+				'OmniWeb'    => array('browser'=>'omniweb',      'shorty'=>'ow', 'engine'=>'webkit',   'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
+				'Silk'       => array('browser'=>'silk',         'shorty'=>'si', 'engine'=>'blink',    'version'=>'/^.*Silk\/(\d+(\.\d+)*).*$/'),
+				'Safari'     => array('browser'=>'safari',       'shorty'=>'sf', 'engine'=>'webkit',   'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
+				'Opera Mini' => array('browser'=>'opera-mini',   'shorty'=>'oi', 'engine'=>'presto',   'version'=>'/^.*Opera Mini\/(\d+(\.\d+)*).*$/'),
+				'Opera Mobi' => array('browser'=>'opera-mobile', 'shorty'=>'om', 'engine'=>'presto',   'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
+				'Opera'      => array('browser'=>'opera',        'shorty'=>'op', 'engine'=>'blink',    'version'=>'/^.*Version\/(\d+(\.\d+)*).*$/'),
+				'IEMobile'   => array('browser'=>'ie-mobile',    'shorty'=>'im', 'engine'=>'trident',  'version'=>'/^.*IEMobile (\d+(\.\d+)*).*$/'),
+				'Camino'     => array('browser'=>'camino',       'shorty'=>'ca', 'engine'=>'gecko',    'version'=>'/^.*Camino\/(\d+(\.\d+)*).*$/'),
+				'Konqueror'  => array('browser'=>'konqueror',    'shorty'=>'ko', 'engine'=>'webkit',   'version'=>'/^.*Konqueror\/(\d+(\.\d+)*).*$/')
 		);
 
 
@@ -245,8 +247,8 @@ if (!class_exists("environment")) {
 			return $ref;
 		}
 
-		public function agent(){
-			$ua = $this->get_useragent();
+		public function agent($strUseragent=false){
+			$ua = ($strUseragent === false) ? $this->get_useragent() : $strUseragent;
 
 			$return = new stdClass();
 			$return->string = $ua;
@@ -315,7 +317,6 @@ if (!class_exists("environment")) {
 			$return->mobile   = $mobile;
 			return $return;
 		}
-
 
 		/**
 		 * Checks if Operating System is Windows IIS
