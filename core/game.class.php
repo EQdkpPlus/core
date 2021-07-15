b2e8a347342684570bfaa8a6a722dedbe4786977
GodMod <GodMod@users.noreply.github.com>
2021-07-15 09:50:41 +0200

    Prevent fatal error

--- core/game.class.php
+++ core/game.class.php
@@ -1328,6 +1328,8 @@ class game extends gen_class {
 				'game_language'		=> $strGamelang,
 				'game_version'		=> $this->gameinfo()->version
 		);
+		
+		$itt_config = array();
 
 		//If write any data
 		if($intOverwriteData !== 0){
